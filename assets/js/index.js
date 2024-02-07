4e46de75294700919bcad9dc0cbcbf845058b345
Sergio Felipe Eliazar Salcedo López <136506920+xoeliazar@users.noreply.github.com>
2024-02-06 21:23:13 -0300

    Upload Challengue

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+precio = 400000;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+function botonSuma() {
+  let valorTotal = document.querySelector(".valor-total");
+  let sumaTotal = Number(valorTotal.innerHTML);
+  sumaTotal = sumaTotal + precio;
+  valorTotal.innerHTML = sumaTotal;
+
+  let valorCantidad = document.querySelector(".cantidad");
+  let cantidadPositiva = Number(valorCantidad.innerHTML);
+  cantidadPositiva = cantidadPositiva + 1;
+  valorCantidad.innerHTML = cantidadPositiva;
+}
+
+function botonResta() {
+  let valorTotal = document.querySelector(".valor-total");
+  let restaTotal = Number(valorTotal.innerHTML);
+  restaTotal = restaTotal - precio;
+  restaTotal = Math.max(0, restaTotal);
+  valorTotal.innerHTML = restaTotal;
+
+  let valorCantidad = document.querySelector(".cantidad");
+  let cantidadNegativa = Number(valorCantidad.innerHTML);
+  cantidadNegativa = cantidadNegativa - 1;
+  cantidadNegativa = Math.max(0, cantidadNegativa);
+  valorCantidad.innerHTML = cantidadNegativa;
+}
